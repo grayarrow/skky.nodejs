1d5044edcef2599fedd51ad81d6cd8e01be5386c
David Dietrich <grayarrow@gmail.com>
2017-12-30 01:42:10 -0800

    skky object added skky.iot and skky.constants.

--- test.js
+++ test.js
@@ -6,3 +6,16 @@ test("ensure hasData works properly", function(t) {
 	t.ok(skky.hasData(s));
 	t.end();
 });
+
+test("ensure iot object can be newed", function(t) {
+	var s = new skky.iot();
+	t.ok(skky.isObject(s));
+	t.end();
+});
+
+test("Config filename", function(t) {
+	var s = skky.constants.ConfigFilename;
+	t.ok(skky.hasData(s));
+	console.log('Config filename:', s);
+	t.end();
+});
